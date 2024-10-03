ef1b4a75677d35d36c55e080d8722b58b1efa7bc
salmxnz <138376829+salmxnz@users.noreply.github.com>
2024-10-04 00:56:57 +0400

    product categories
    to fix: when product active, padding appears grey. background-transparent set yet still bruh idk. must debug. dropdown doesn't look good in mobile. must fix

--- assets/js/navbarToggle.js
+++ assets/js/navbarToggle.js
@@ -9,3 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const dropdownToggle = document.getElementById("navbarDropdownMenuLink");
+//   const dropdownMenu = dropdownToggle.nextElementSibling; // The dropdown menu
+
+//   dropdownToggle.addEventListener("click", function (event) {
+//     event.preventDefault(); // Prevent default anchor behavior
+//     dropdownMenu.classList.toggle("show"); // Toggle dropdown visibility
+//   });
+// });
